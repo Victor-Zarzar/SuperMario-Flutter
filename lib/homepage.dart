8f38c9f9ff5b1e236ac5d0729b7f2ea35b0ef051
victorzarzar <victorzarzar@pop-os.localdomain>
2023-06-11 16:23:48 -0300

    updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:supermario/mario.dart';
+import 'button.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +10,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.blue,
+              child: AnimatedContainer(
+                alignment: Alignment(0, 0),
+                duration: Duration(milliseconds: 0),
+                child: MyMario(),
+              ),
+            )),
+        Expanded(
+          flex: 1,
+          child: Container(
+            color: Colors.brown,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MyButton(
+                    child: Icon(Icons.arrow_back, color: Colors.white),
+                  ),
+                  MyButton(
+                    child: Icon(Icons.arrow_upward, color: Colors.white),
+                  ),
+                  MyButton(
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
+                  ),
+                ]),
+          ),
+        )
+      ],
+    ));
   }
 }
